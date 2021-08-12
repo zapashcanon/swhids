0bbd7df13b574cdc9811ac5806a2147ddf1454cb
zapashcanon <leo@ndrs.fr>
2021-08-12 15:12:13 +0200

    debug windows

--- test/test_download.ml
+++ test/test_download.ml
@@ -1,12 +1,12 @@
 (* test content *)
 let () =
-  match
-    Swhids.Download.content ~hash_type:"sha1_git"
-      "7bdf38d4468c114206c9b6ebd9cf1176e085d346"
-  with
+  let id = "7bdf38d4468c114206c9b6ebd9cf1176e085d346" in
+  match Swhids.Download.content ~hash_type:"sha1_git" id with
   | Ok result ->
     assert (
       result
       = Leaf
           "https://archive.softwareheritage.org/api/1/content/sha1_git:7bdf38d4468c114206c9b6ebd9cf1176e085d346/raw/" )
-  | Error _e -> assert false
+  | Error e ->
+    Format.eprintf "Test for id `%s` failed with error `%s`@." id e;
+    assert false
